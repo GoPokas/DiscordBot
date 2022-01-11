d5ed8dacbc67354fc8f53f39bf7f9c5eb921f62e
Rodrigo Alves <pocas.alves@gmail.com>
2022-01-11 03:03:23 +0000

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const TOKEN = "OTMwMjc3MDU3OTQwODI4MjAw.YdziBg.NdLisYVq67JcHIFSj1aiddd95cw";
+const bottoken = process.env.token;
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
@@ -12,9 +12,21 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content === "") {
-    message.reply("Dá-me magos elétricos.");
+  if (message.author.bot) return false;
+
+  if (message.mentions.has(client.user.id)) {
+    //Choose a random number between 1 and 3
+    const random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+      message.reply("Dá-me magos elétricos.");
+    } else if (random === 2) {
+      message.reply("Famum bico.");
+    } else if (random === 3) {
+      message.reply("*Começa a tremer que nem um maluco*");
+    }
+  } else if (message.content == "<@!303902144027820043>") {
+    message.channel.send("Oh Castor Informático, famum bico.");
   }
 });
 
-client.login(TOKEN);
+client.login(bottoken);
