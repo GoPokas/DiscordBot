7b0f9efe7caa5bfde584b73125e717f9d8163818
Rodrigo Alves <pocas.alves@gmail.com>
2022-01-12 00:33:32 +0000

    Updates

--- index.js
+++ index.js
@@ -1,32 +1,79 @@
 const Discord = require("discord.js");
-require("dotenv").config();
+// const DiscordVoice = require("@discordjs/voice");
 
-const bottoken = process.env.token;
+const bottoken = "OTMwMjc3MDU3OTQwODI4MjAw.YdziBg.dfq9JkLLGdup49VK5zHboTCWzXA";
+
+const CastroID = "303902144027820043";
+const PeixeID = "605848360175403270";
+const TalinID = "486887211837685778";
+const PokasID = "247483052370952192";
+const DavidID = "684806232636260414";
+
+const prefix = ",";
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
 
+//Login check
 client.on("ready", () => {
   console.log("Logged in as " + client.user.tag);
 });
 
+//Random message when pinged
 client.on("messageCreate", (message) => {
   if (message.author.bot) return false;
 
-  if (message.mentions.has(client.user.id)) {
+  if (message.mentions.has(client.user.id) && message.content.includes("")) {
     //Choose a random number between 1 and 3
     const random = Math.floor(Math.random() * 3) + 1;
-    if (random === 1) {
+    let msg = message.content;
+    message.content = msg.toLowerCase();
+    if (message.content.includes("mario")) {
+      message.channel.send("É quem comeu a tua mãe atrás do armário");
+    } else if (random === 1) {
       message.reply("Dá-me magos elétricos.");
     } else if (random === 2) {
       message.reply("Famum bico.");
     } else if (random === 3) {
       message.reply("*Começa a tremer que nem um maluco*");
     }
-  } else if (message.content == "<@!303902144027820043>") {
-    message.channel.send("Oh Castor Informático, famum bico.");
   }
 });
 
+//Specific messages depending on who is pinged
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return false;
+
+  if (message.mentions.has(CastroID)) {
+    message.channel.send("Oh Castor Informático, famum bico.");
+  }
+  if (message.mentions.has(TalinID) || message.mentions.has(PeixeID)) {
+    message.channel.send("Entra aí no meu clã.");
+  }
+});
+
+//Returns response time when pinged
+client.on("messageCreate", (message) => {
+  if (
+    message.content.includes === "ping" ||
+    message.content.startsWith(prefix)
+  ) {
+    message.channel.send(
+      `Estou a lagar: ${
+        Date.now() - message.createdTimestamp
+      }ms. Que é isto sequer(API): ${Math.round(client.ws.ping)}ms`
+    );
+  }
+});
+
+//Live server
 client.login(bottoken);
+
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end();
+  })
+  .listen(8080);
