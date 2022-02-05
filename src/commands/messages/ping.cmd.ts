3ccf77e08ee77f2aca373d2880de76281c7593c9
GoPokas <pocas.alves@gmail.com>
2022-02-05 23:56:21 +0000

    Update

---
+++ src/commands/messages/ping.cmd.ts
@@ -0,0 +1,7 @@
+import { Message } from "discord.js";
+import { Command } from "discord.js-commando";
+import { Discord, Slash, SlashOption, SlashChoice } from "discordx";
+
+@Discord()
+class PingCommand {
+  }
