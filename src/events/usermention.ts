f1b909bbc7ebe24d999d47bf79a3421ceb2aa633
diogocastrodev <diogocastro.dev@gmail.com>
2022-02-09 12:41:46 +0000

    Auto Detect User mention

--- src/events/usermention.ts
+++ src/events/usermention.ts
@@ -27,48 +27,5 @@ export abstract class UserMention {
 						}
 					});
 			}
-
-		/* if (message.content.includes(config.userIDs.CastroID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options?.user.find((user) => user.id === config.userIDs.CastroID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-			if (message.content.includes(config.userIDs.DavidID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options?.user.find((user) => user.id === config.userIDs.DavidID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-			if (message.content.includes(config.userIDs.PapiroID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.PapiroID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-			if (message.content.includes(config.userIDs.PeixeID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.PeixeID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-			if (message.content.includes(config.userIDs.PokasID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.PokasID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-			if (message.content.includes(config.userIDs.TalinID)) {
-				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.TalinID)
-				);
-				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
-				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			} */
 	}
 }
