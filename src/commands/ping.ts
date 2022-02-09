95649bedc1095442f6a7348792327ea12c8c0ebc
GoPokas <pocas.alves@gmail.com>
2022-02-09 12:00:37 +0000

    Updated /ping embed

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,12 +4,23 @@ import { Client, Discord, Slash } from 'discordx';
 @Discord()
 export abstract class PingCommand {
 	@Slash('ping', {
-		description: 'Pong! :ping_pong:'
+		description: 'Pong!'
 	})
 	private async ping(message: Message, client: Client): Promise<Message> {
 		const embed = new MessageEmbed()
+			.setTitle('Pong!')
 			.setColor('#0099ff')
-			.setDescription(`Pong! \`${client.ws.ping}ms\``);
+			.setFields(
+				{
+					name: 'Bot',
+					value: `\`${Date.now() - message.createdTimestamp}ms\``
+				},
+				{
+					name: 'API',
+					value: `\`${client.ws.ping}ms\``
+				}
+			)
+			.setFooter({ text: `Requested by ${message.author.tag}` });
 
 		return message.reply({ embeds: [embed] });
 
