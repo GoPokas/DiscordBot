2ad22366be9f409ea1065b00e85ee9672931a9fe
diogocastrodev <diogocastro.dev@gmail.com>
2022-02-09 12:38:28 +0000

    Auto Detect User mention

--- src/events/usermention.ts
+++ src/events/usermention.ts
@@ -11,17 +11,33 @@ export abstract class UserMention {
 		client: Client, // Client instance injected here,
 		_guardPayload: GuardFunction
 	): Promise<void> {
-		if (client.user) {
-			if (message.content.includes(config.userIDs.CastroID)) {
+		if (client.user)
+			if (message.mentions) {
 				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.CastroID)
+					fil.options?.user.find((user) => message.mentions.has(user.id))
+				);
+
+				const randomNumber: number = genRandomNumber(1, msgs.length) - 1;
+
+				msgs.length > 0 &&
+					message.reply({
+						content: msgs[randomNumber].message,
+						allowedMentions: {
+							repliedUser: false
+						}
+					});
+			}
+
+		/* if (message.content.includes(config.userIDs.CastroID)) {
+				const msgs = messagesWithUser.filter((fil) =>
+					fil.options?.user.find((user) => user.id === config.userIDs.CastroID)
 				);
 				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
 				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
 			}
 			if (message.content.includes(config.userIDs.DavidID)) {
 				const msgs = messagesWithUser.filter((fil) =>
-					fil.options.user.find((user) => user.id === config.userIDs.DavidID)
+					fil.options?.user.find((user) => user.id === config.userIDs.DavidID)
 				);
 				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
 				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
@@ -53,7 +69,6 @@ export abstract class UserMention {
 				);
 				const rdmNumber = genRandomNumber(1, msgs.length) - 1;
 				msgs.length > 0 && message.reply(msgs[rdmNumber].message);
-			}
-		}
+			} */
 	}
 }
