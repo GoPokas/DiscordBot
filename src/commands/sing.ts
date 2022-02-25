b577292277ef9e0eb35e5785ea662d7a65d945fb
Rodrigo Alves <pocas.alves@gmail.com>
2022-02-25 12:59:33 +0000

    Added /sing command

---
+++ src/commands/sing.ts
@@ -0,0 +1,18 @@
+import { Message, MessageAttachment } from 'discord.js';
+import { Client, Discord, Slash } from 'discordx';
+
+@Discord()
+export abstract class SingCommand {
+	@Slash('sing', {
+		description: 'Sing!'
+	})
+	private async sing(message: Message, client: Client): Promise<Message> {
+		const attachment = new MessageAttachment('./assets/dami.mp4');
+		return message.reply({
+			files: [attachment],
+			allowedMentions: {
+				repliedUser: false
+			}
+		});
+	}
+}
